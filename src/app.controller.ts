13e31222a96dbe7fcc53ca2ee34207bb22760766
josuj0315-oss <josujeong@joyun-ui-MacBookAir.local>
2026-03-20 08:30:06 +0900

    chore: remove Sentry test route /sentry-test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import * as Sentry from '@sentry/nestjs';
 
 @Controller()
 export class AppController {
@@ -9,13 +8,5 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  // 배포 Sentry 연동 확인용 임시 라우트 (운영 확인 후 즉시 본 메서드 제거 요망)
-  @Get('sentry-test')
-  testSentry(): never {
-    const error = new Error('Production backend test');
-    Sentry.captureException(error);
-    throw error;
   }
 }
